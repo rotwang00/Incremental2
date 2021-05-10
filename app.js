0de911ed2d6d003082b75924a23478f4a28b12a0
Paul Thurmond <paulthurmond@gmail.com>
2021-05-10 13:00:14 -0500

    Two working buttons

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-let money = 10;
+let money = 0;
 let increment = 1;
+let improvementCost = 10;
 
 const moneyDisplay = document.getElementById("moneyDisplay");
 const moneyButton = document.getElementById("makeMoney");
@@ -10,9 +11,16 @@ improveFirstButton.addEventListener("click", () => { improve(); }, false);
 
 function addMoney() {
     money += increment;
-    moneyDisplay.innerHTML = ("You have " + money + " dollars");
+    moneyDisplay.innerHTML = `You have $${money}`;
 }
 
 function improve() {
+    if (improvementCost > money) return;
+
+    money -= improvementCost;
     increment *= 2;
+    improvementCost *= 5;
+    moneyButton.innerHTML = `Make $${increment}`;
+    improveFirstButton.innerHTML = `Improve first button for $${improvementCost}`;
+    moneyDisplay.innerHTML = `You have $${money}`;
 }
